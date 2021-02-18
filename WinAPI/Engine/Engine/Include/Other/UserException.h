15458a54ad1eb235783cfd1dc6d82d49be8f9dad
hyojin yoon <40716234+jinsnowy@users.noreply.github.com>
2021-02-18 13:22:26 +0900

    make Exception class's what() to final

--- WinAPI/Engine/Engine/Include/Other/UserException.h
+++ WinAPI/Engine/Engine/Include/Other/UserException.h
@@ -12,7 +12,7 @@ private:
 public:
 	UserException(int codeLine, const char* fileName) noexcept;
 	// 예외 발생시 상세 내용 출력 인터페이스 
-	virtual const char* what() const noexcept override;
+	virtual const char* what() const noexcept final;
 	virtual const char* GetType() const noexcept;
 	virtual std::string GetErrorMessage() const noexcept;
 };
