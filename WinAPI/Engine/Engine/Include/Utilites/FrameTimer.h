45dfb457a4eadc842974dd188335a144d43f5085
hyojin yoon <40716234+jinsnowy@users.noreply.github.com>
2021-02-18 19:47:01 +0900

    add TimeScale

--- WinAPI/Engine/Engine/Include/Utilites/FrameTimer.h
+++ WinAPI/Engine/Engine/Include/Utilites/FrameTimer.h
@@ -7,8 +7,11 @@ class FrameTimer
 	DECLARE_SINGLE(FrameTimer)
 public:
 	bool Init();
+	void SetTimeScale(float timeScale) { m_TimeScale = timeScale; }
+
 	float Peek();
 	float Tick();
 private:
-	std::chrono::steady_clock::time_point mTic;
+	std::chrono::steady_clock::time_point m_Tic;
+	float m_TimeScale;
 };
