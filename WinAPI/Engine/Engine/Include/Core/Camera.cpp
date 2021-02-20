aaa9bcca8b956de71c90d55fbaff87667ac63da8
hyojin yoon <40716234+jinsnowy@users.noreply.github.com>
2021-02-20 22:40:54 +0900

    add Camera class

---
+++ WinAPI/Engine/Engine/Include/Core/Camera.cpp
@@ -0,0 +1,104 @@
+#include "Camera.h"
+#include "../Object/Object.h"
+
+DEFINITION_SINGLE(Camera)
+
+Camera::Camera()
+	:
+	m_tPos(0.f,0.f),
+	m_tClientRS(0, 0),
+	m_tWorldRS(0, 0),
+	m_tPivot(0.5f, 0.5f),
+	m_pTarget(nullptr)
+{
+}
+
+Camera::~Camera()
+{
+	SAFE_RELEASE(m_pTarget);
+}
+
+bool Camera::Init(const Pos& tPos, const RESOLUTION& tRS, const RESOLUTION& tWorldRS)
+{
+	m_tPos = tPos;
+	m_tClientRS = tRS;
+	m_tWorldRS = tWorldRS;
+	m_tPivot = Pos(0.5f, 0.5f);
+	return true;
+}
+
+void Camera::Input(float dt)
+{
+	if (m_pTarget)
+	{
+		m_tPos = m_pTarget->GetPos();
+		Pos offset = m_tPivot * m_tClientRS;
+		Pos cameraTopLeft = m_tPos - offset;
+		Pos cameraBottomRight = cameraTopLeft + m_tClientRS;
+		if (cameraTopLeft.x < 0)
+		{
+			m_tPos.x = offset.x;
+		}
+		else if (cameraBottomRight.x >= m_tWorldRS.x)
+		{
+			m_tPos.x = m_tWorldRS.x - (m_tClientRS.x - offset.x);
+		}
+		if (cameraTopLeft.y < 0)
+		{
+			m_tPos.y = offset.y;
+		}
+		else if (cameraBottomRight.y >= m_tWorldRS.y)
+		{
+			m_tPos.y = m_tWorldRS.y - (m_tClientRS.y - offset.y);
+		}
+		
+		/*Pos tPos   =  m_pTarget->GetPos();
+		Pos tPivot = m_pTarget->GetPivot();
+		Size tSize = m_pTarget->GetSize();
+
+		Pos targetTopLeft = tPos - tPivot * tSize;
+		Pos targetBottomRight = targetTopLeft + tSize;
+
+		float fLeftArea = m_tClientRS.x * m_tPivot.x;
+		float fRightArea = m_tClientRS.x - fLeftArea;
+		float fTopArea = m_tClientRS.y * m_tPivot.y;
+		float fBottomArea = m_tClientRS.y - fTopArea;
+	
+		if (targetTopLeft.x <= fLeftArea)
+		{
+			m_tPos.x = 0.f;
+		}
+		else if (targetBottomRight.x >= m_tWorldRS.x - fRightArea)
+		{
+			m_tPos.x = m_tWorldRS.x - m_tClientRS.x;
+		}
+		else {
+			m_tPos.x = tPos.x - m_tClientRS.x * m_tPivot.x;
+		}
+
+		if (targetTopLeft.y <= fTopArea)
+		{
+			m_tPos.y = 0.f;
+		}
+		else if (targetBottomRight.y >= m_tWorldRS.y - fBottomArea)
+		{
+			m_tPos.y = m_tWorldRS.y - m_tClientRS.y;
+		}
+		else {
+			m_tPos.y = tPos.y - m_tClientRS.y * m_tPivot.y;
+		}*/
+	}
+}
+
+void Camera::Update(float dt)
+{
+}
+
+void Camera::SetTarget(Object* pTarget)
+{
+	SAFE_RELEASE(m_pTarget);
+	m_pTarget = pTarget;
+
+	if (m_pTarget)
+		m_pTarget->AddRef();
+}
---
+++ WinAPI/Engine/Engine/Include/Core/Camera.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "../framework.h"
+
+class Camera
+{
+	DECLARE_SINGLE(Camera)
+private:
+	Pos			m_tPos;
+	Pos			m_tPivot;
+	RESOLUTION	m_tClientRS;
+	RESOLUTION	m_tWorldRS;
+	class Object* m_pTarget;
+
+public:
+	bool Init(const Pos& tPos, const RESOLUTION& tRS, const RESOLUTION& tWorldRS);
+	void Input(float dt);
+	void Update(float dt);
+	
+	Pos GetPos() const { return m_tPos; }
+	Pos GetPivot() const { return m_tPivot; }
+	Pos GetTopLeft() const { return m_tPos - m_tPivot * m_tClientRS; }
+	RESOLUTION GetClientRS() const { return m_tClientRS; }
+
+	void SetTarget(class Object* pTarget);
+	void SetPos(const Pos& pos)   { m_tPos = pos; }
+	void SetPos(float x, float y) { m_tPos = Pos(x, y); }
+	void SetPivot(const Pos& pivot) { m_tPivot = pivot; }
+	void SetPivot(float x, float y) { m_tPivot.x = x; m_tPivot.y = y; }
+	void SetClientResolution(const RESOLUTION& tRS)		{ m_tClientRS = tRS; }
+	void SetWorldResolution(const RESOLUTION& tWorldRS) { m_tWorldRS = tWorldRS; }
+};
+
