865a10db3ec19d3c239ec7ec797bcb96aa94b242
hyojin yoon <40716234+jinsnowy@users.noreply.github.com>
2021-02-21 14:23:39 +0900

    add enum collider type

--- WinAPI/Engine/Engine/Include/Types/Types.h
+++ WinAPI/Engine/Engine/Include/Types/Types.h
@@ -1,8 +1,20 @@
 #pragma once
 
+// Move dir type
 enum MOVE_DIR
 {
 	MD_BACK = -1,
 	MD_NONE,
 	MD_FRONT
+};
+
+// Collider Type
+enum COLLIDER_TYPE
+{
+	CT_RECT,
+	CT_SPHERE,
+	CT_LINE,
+	CT_POINT,
+	CT_PIXEL,
+	CT_END
 };
