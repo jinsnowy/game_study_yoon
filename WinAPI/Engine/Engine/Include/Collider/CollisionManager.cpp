8a1fc24b2a2d4d0840a6917d8719f88aab24aeee
hyojin yoon <40716234+jinsnowy@users.noreply.github.com>
2021-02-21 20:22:28 +0900

    add CollisionManager

--- WinAPI/Engine/Engine/Include/Collider/CollisionManager.cpp
+++ WinAPI/Engine/Engine/Include/Collider/CollisionManager.cpp
@@ -1,4 +1,8 @@
 #include "CollisionManager.h"
+#include "../Object/Object.h"
+#include "Collider.h"
+
+DEFINITION_SINGLE(CollisionManager);
 
 CollisionManager::CollisionManager()
 {
@@ -11,4 +15,30 @@ CollisionManager::~CollisionManager()
 bool CollisionManager::Init()
 {
     return true;
+}
+
+void CollisionManager::AddObject(Object* pObj)
+{
+    if (pObj->CheckCollider())
+    {
+        m_CollisionList.push_back(pObj);
+    }
+}
+
+void CollisionManager::Collision(float dt)
+{
+    if (m_CollisionList.size() < 2)
+    {
+        m_CollisionList.clear();
+        return;
+    }
+
+    // 오브젝트 간 충돌 처리를 한다.
+    
+    
+    
+    
+    
+    
+    m_CollisionList.clear();
 }
--- WinAPI/Engine/Engine/Include/Collider/CollisionManager.h
+++ WinAPI/Engine/Engine/Include/Collider/CollisionManager.h
@@ -4,6 +4,10 @@
 class CollisionManager
 {
 DECLARE_SINGLE(CollisionManager)
+private:
+	list <class Object*> m_CollisionList;
 public:
 	bool Init();
+	void AddObject(class Object* pObj);
+	void Collision(float dt);
 };
